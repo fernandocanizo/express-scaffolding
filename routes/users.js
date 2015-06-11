a89a52ed38475b16d7f8512d1d53eeac3ccdc076
Fernando L. Canizo <flc@muriandre.com>
2015-06-11 07:44:49 -0300

    Use tabs, not spaces
    Also remove unused function to detect if port is provided as a string:
    provide the proper type and don't add posibly never to be used code.

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+	// GET users listing
+
+	res.send('Unimplemented.');
 });
 
 module.exports = router;
