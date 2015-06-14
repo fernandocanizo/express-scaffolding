a4bb7aecabc1589fd523b3864324ca3229d55cc1
Fernando L. Canizo <flc@muriandre.com>
2015-06-14 17:11:42 -0300

    Bootstrap Blog example in Jade

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,45 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	// home page
 
-	res.render('index', { title: 'Express' });
+	// pass request URL to template
+	res.render('index', {
+		title: 'Blog Template',
+		url: req.url
+	});
+});
+
+// menu routes to show different active menu, but providing same content
+// (this is just a demo after all)
+
+router.get("/new-features", function (req, res, next) {
+	res.render('index', {
+		title: 'Blog Template',
+		url: req.url
+	});
+});
+
+router.get("/press", function (req, res, next) {
+	res.render('index', {
+		title: 'Blog Template',
+		url: req.url
+	});
+});
+
+router.get("/new-hires", function (req, res, next) {
+	res.render('index', {
+		title: 'Blog Template',
+		url: req.url
+	});
+});
+
+router.get("/about", function (req, res, next) {
+	res.render('index', {
+		title: 'Blog Template',
+		url: req.url
+	});
 });
 
 
